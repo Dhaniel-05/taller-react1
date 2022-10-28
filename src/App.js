2774c97368fbc8b7dac172fc572110633889da8c
Daniel Payan <dhanymarth@gmail.com>
2022-10-28 16:42:53 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import logo from './img/SENA_logo.png';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter]=useState(0);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ejercicio1 <code>:</code> Esto es un contador.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <section className='contador'>
+        <p>{counter}</p>
+      </section>
+      <section className='botones'>
+        <button onClick={() =>
+          {if(counter == 0){
+            alert("No puedes disminuir mas");
+          }else{
+            setCounter(counter-1)
+          }}
+        } className='botones-disminuir'>
+          Disminuir
+        </button>
+        <button onClick={() => setCounter(0)} className='botones-disminuir'>
+          Reiniciar
+        </button>
+        <button onClick={() => setCounter(counter + 1)} className='botones-disminuir'>
+          Aumentar
+        </button>
+      </section>
       </header>
     </div>
   );
